a936e7cf63be64075136aae2f5fdeaf933623130
shubhampatel27 <shubhampateldz@gmail.com>
2023-01-29 23:44:52 +0530

    29/01/2023

---
+++ stockBuyAndSell06.cpp
@@ -0,0 +1,15 @@
+// #include <bits/stdc++.h> 
+// int maximumProfit(vector<int> &prices){
+//     // Write your code here.
+//     if(prices.size()==1) return 0;
+//     int mini = prices[0];
+//     int maxi_profit = 0;
+    
+//     for(int i = 1; i<prices.size(); i++){
+//         mini = min(mini,prices[i]);
+//         maxi_profit = max(maxi_profit, prices[i]-mini);
+//     }
+    
+//     return maxi_profit;
+    
+// }
