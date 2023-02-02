689f1810786970c58bbc1a3f71ef6734b4a03f22
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-02 23:33:33 +0530

    02/02/2023

---
+++ findDuplicate10.cpp
@@ -0,0 +1,15 @@
+// #include <bits/stdc++.h> 
+// #include <bits/stdc++.h> 
+// int findDuplicate(vector<int> &arr, int n){
+// 	// Write your code here.
+//     int ans  =-1;
+//     sort(arr.begin(),arr.end());
+//     for(int i=0; i<n-1; i++)
+//     {
+//         if(arr[i]==arr[i+1]){
+//             ans=arr[i];
+//             break;
+//         }
+//     }
+//     return ans;
+// }
